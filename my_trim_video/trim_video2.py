85aa8ce932a079d529285897f102ec3ecd68a7aa
Marco T <marcotts@gmail.com>
2021-09-21 15:56:14 +0200

    update

---
+++ my_trim_video/trim_video2.py
@@ -0,0 +1,12 @@
+import sys
+import ffmpeg
+
+from my_trim_video.trim_video_length import show_info
+
+input_video_file_name = '/media/marco/76E8-CACF/media/Meeting-20210914_103237-Meeting Recording.mp4'
+
+output_video_file_name = '/media/marco/76E8-CACF/media/video-out.mkv'
+
+
+show_info(input_video_file_name)
+
